31b2423e7d9caaa3a080ba7dada72e5efcb350b9
Vi Hoàng <vi.hoang0706@gmail.com>
2020-05-29 22:16:04 +0700

    Edit file pom and add gitignore

--- src/test/java/com/logigear/tranining/user/management/TC_001_LoginPage_VerifyLoginSuccessfully.java
+++ src/test/java/com/logigear/tranining/user/management/TC_001_LoginPage_VerifyLoginSuccessfully.java
@@ -1,4 +1,15 @@
 package com.logigear.tranining.user.management;
 
+import org.testng.annotations.Test;
+
 public class TC_001_LoginPage_VerifyLoginSuccessfully {
+    @Test
+    public void TC_001() {
+        System.out.println("TC_001");
+    }
+
+    @Test
+    public void TC_002() {
+        System.out.println("TC_001");
+    }
 }
