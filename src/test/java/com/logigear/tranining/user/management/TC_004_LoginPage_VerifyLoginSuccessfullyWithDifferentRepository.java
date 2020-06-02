9a8b09157d2919b456735b8fe0aa4b31720a5ed4
htran <thuhang09cntt2@gmail.com>
2020-06-03 00:54:18 +0700

    implement TC005

--- src/test/java/com/logigear/tranining/user/management/TC_004_LoginPage_VerifyLoginSuccessfullyWithDifferentRepository.java
+++ src/test/java/com/logigear/tranining/user/management/TC_004_LoginPage_VerifyLoginSuccessfullyWithDifferentRepository.java
@@ -2,7 +2,7 @@ package com.logigear.tranining.user.management;
 
 import com.logigear.training.common.GlobalVariables;
 import com.logigear.training.common.TestBase;
-import com.logigear.training.page.HomePage;
+import com.logigear.training.page.DashboardPage;
 import com.logigear.training.page.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class TC_004_LoginPage_VerifyLoginSuccessfullyWithDifferentRepository extends TestBase {
 
     LoginPage loginPage = new LoginPage();
-    HomePage homepage = new HomePage();
+    DashboardPage dashboardPage = new DashboardPage();
 
     @Test
     public void TC_001() {
@@ -19,7 +19,7 @@ public class TC_004_LoginPage_VerifyLoginSuccessfullyWithDifferentRepository ext
         loginPage.login(GlobalVariables.VALID_USERNAME, GlobalVariables.VALID_PASSWORD);
 
         //Logout
-        homepage.logout();
+        dashboardPage.logout();
 
         //Select another repository
         loginPage.selectRepository(GlobalVariables.SAMPLE_REPOSITORY_LV2);
@@ -28,7 +28,7 @@ public class TC_004_LoginPage_VerifyLoginSuccessfullyWithDifferentRepository ext
         loginPage.login(GlobalVariables.VALID_USERNAME, GlobalVariables.VALID_PASSWORD);
 
         //Verify that user login to SampleRepositoryLV2 successfully
-        Assert.assertEquals(homepage.getWelcomeAccount(), GlobalVariables.VALID_USERNAME);
-        Assert.assertEquals(homepage.getRepository(), GlobalVariables.SAMPLE_REPOSITORY_LV2);
+        Assert.assertEquals(dashboardPage.getWelcomeAccount(), GlobalVariables.VALID_USERNAME);
+        Assert.assertEquals(dashboardPage.getRepository(), GlobalVariables.SAMPLE_REPOSITORY_LV2);
     }
 }
