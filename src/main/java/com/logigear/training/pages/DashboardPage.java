ee84d1e2ca77e211b1da1db287554b3df2ce036d
Vi Hoàng <vi.hoang0706@gmail.com>
2020-06-22 22:46:48 +0700

    Add TC_013

--- src/main/java/com/logigear/training/pages/DashboardPage.java
+++ src/main/java/com/logigear/training/pages/DashboardPage.java
@@ -1,20 +1,24 @@
 package com.logigear.training.pages;
 
 import com.logigear.training.utilities.DriverUtils;
-import com.logigear.training.utilities.controls.LGLabel;
-import com.logigear.training.utilities.controls.LGLink;
+import com.logigear.training.utilities.controls.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class DashboardPage {
 
     LGLink lnkLogout = new LGLink(By.xpath("//a[.='Logout']"));
     LGLabel lblWelcomeAccount = new LGLabel(By.xpath("//a[@href='#Welcome']"));
     LGLabel lblRepository = new LGLabel(By.xpath("//a[@href='#Repository']/span"));
-    LGLabel lblSubMenu = new LGLabel(By.xpath("//a[contains(text(),'%s')]"));
+    private String lblSubMenu = "//a[contains(text(),'%s')]";
+    public By lblGlobalSetting = By.xpath("//li[@class='mn-setting']/a");
+    LGTextBox txtPageName = new LGTextBox(By.id("#name"));
+    LGButton btnOk = new LGButton(By.id("#OK"));
+    LGSelect cbbDisplayAfter = new LGSelect(By.xpath("//select[@id='afterpage']/option[contains(text(),'%s')]"));
 
     protected WebElement getSubMenu(String tabName) {
-        return lblSubMenu.formatDynamicLocator(tabName);
+        return DriverUtils.getDriver().findElement(By.xpath(String.format(lblSubMenu,tabName)));
     }
 
     public String getRepository() {
@@ -34,5 +38,27 @@ public class DashboardPage {
     public void switchRepository(String repo){
         this.getSubMenu("Repository").click();
         DriverUtils.getDriver().findElement(By.partialLinkText(repo)).click();
+    }
+
+    public void goToAddPage() {
+        DriverUtils.getDriver().findElement(lblGlobalSetting).click();
+        this.getSubMenu("Add Page").click();
+    }
+
+    public void addPage(String pageName) {
+        this.setPageName(pageName);
+        this.clickOk();
+    }
+
+    public void setPageName(String pageName) {
+        txtPageName.enter(pageName);
+    }
+
+    public void clickOk() {
+        btnOk.click();
+    }
+
+    public void selectDisplayAfter(String pageName) {
+       cbbDisplayAfter.select(pageName);
     }
 }
---
+++ src/test/java/com/logigear/training/main/page/TC_013_MainPage_VerifyTheNewlyMainParentPageIsPositionedAtLocationSpecificed.java
@@ -0,0 +1,46 @@
+package com.logigear.training.main.page;
+
+import com.aventstack.extentreports.Status;
+import com.logigear.training.common.Constants;
+import com.logigear.training.pages.DashboardPage;
+import com.logigear.training.pages.LoginPage;
+import com.logigear.training.test.base.TestBase;
+import com.logigear.training.utilities.DriverUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class TC_013_MainPage_VerifyTheNewlyMainParentPageIsPositionedAtLocationSpecificed extends TestBase {
+    public LoginPage loginPage = new LoginPage();
+    public DashboardPage dashboardPage = new DashboardPage();
+
+    @Test(description = "Verify that the newly added main parent page is positioned at the location specified as set with \"Displayed After\" field of \"New Page\" form on the main page bar/\"Parent Page\" dropped down menu")
+    public void DA_LOGIN_TC013() throws IOException {
+
+        //Main Steps
+        logClass.log(Status.INFO, "Step #1. Navigate to Dashboard login page");
+        navigateToTestSite(Constants.AUT);
+
+        logClass.log(Status.INFO, "Step #2. Login with valid username and password");
+        loginPage.login(Constants.VALID_USERNAME, Constants.VALID_PASSWORD);
+
+        logClass.log(Status.INFO, "Step #3. Wait for page load");
+        DriverUtils.waitForPageLoaded();
+
+        logClass.log(Status.INFO, "Step #4. Go to Global Setting -> Add page");
+        dashboardPage.goToAddPage();
+
+        DriverUtils.getDriver().switchTo().frame("New Page");
+
+        logClass.log(Status.INFO, "Step #5. Add first page");
+        dashboardPage.addPage("Page1");
+
+        logClass.log(Status.INFO, "Step #6. Go to Global Setting -> Add page");
+        dashboardPage.goToAddPage();
+
+        logClass.log(Status.INFO, "Step #7. Add second page with specific display after");
+        dashboardPage.selectDisplayAfter("Page1");
+        dashboardPage.addPage("Page2");
+    }
+}
+
