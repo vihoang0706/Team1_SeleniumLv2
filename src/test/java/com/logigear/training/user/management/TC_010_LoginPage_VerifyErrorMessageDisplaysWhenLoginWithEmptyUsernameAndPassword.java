9cae90e4adc05f286a733236832cbebd1e669eb5
Thao Dinh <thao.dinh@logigear.com>
2020-06-22 18:02:03 +0700

    update TC_010

--- src/test/java/com/logigear/training/user/management/TC_010_LoginPage_VerifyErrorMessageDisplaysWhenLoginWithEmptyUsernameAndPassword.java
+++ src/test/java/com/logigear/training/user/management/TC_010_LoginPage_VerifyErrorMessageDisplaysWhenLoginWithEmptyUsernameAndPassword.java
@@ -10,13 +10,15 @@ import com.logigear.training.utilities.controls.LGAlert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class TC_010_LoginPage_VerifyErrorMessageDisplaysWhenLoginWithEmptyUsernameAndPassword extends TestBase {
 
     public LoginPage loginPage = new LoginPage();
     public LGAlert alert = new LGAlert();
 
     @Test(description = "Verify that the page works correctly for the case when no input entered to Password and Username field")
-    public void DA_LOGIN_TC010() {
+    public void DA_LOGIN_TC010()  throws IOException {
 
         //Main Steps
         logClass.log(Status.INFO, "Step #1. Navigate to Dashboard login page");
@@ -31,8 +33,7 @@ public class TC_010_LoginPage_VerifyErrorMessageDisplaysWhenLoginWithEmptyUserna
         alert.waitForAlertPresent();
 
         logClass.log(Status.INFO, "Step #3. Verify that Dashboard Error message \"Please enter username!\" appears");
-        String actualErrorMessage = loginPage.getErrorMessage();
-        Assert.assertEquals(actualErrorMessage,Constants.EMPTY_USERNAME_AND_PASSWORD_MSG);
+        DriverUtils.verifyExpectedAndActualResults(logClass,loginPage.getErrorMessage(),Constants.EMPTY_USERNAME_AND_PASSWORD_MSG);
 
         logClass.log(Status.INFO, "Clean up");
         alert.acceptAlert();
